aea30b7e217f55183177b35db0f5bc213ce1bd2d
Gabriel Peres Bernes <bernes.dev@gmail.com>
2026-04-24 19:50:10 +0200

    refactor: destination skeleton layout

--- src/features/location/components/DestinationSkeleton.tsx
+++ src/features/location/components/DestinationSkeleton.tsx
@@ -6,10 +6,10 @@ export function DestinationSkeleton({ className }: { className?: string }) {
   return (
     <View className={`flex-row mb-2 px-3 ${className}`}>
       <View className="w-1/2 pr-1">
-        <AppSkeleton width={'100%'} height={220} />
+        <AppSkeleton width={'100%'} height={220} radius={10} />
       </View>
       <View className="w-1/2 pl-1">
-        <AppSkeleton width={'100%'} height={220} />
+        <AppSkeleton width={'100%'} height={220} radius={10} />
       </View>
     </View>
   );
